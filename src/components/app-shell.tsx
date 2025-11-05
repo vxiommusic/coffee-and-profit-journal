5d672280acbe48da5710cda4efe83c06d68e3360
VXIOM MUSIC <vxiommusic@gmail.com>
2025-11-05 22:35:55 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -56,15 +56,16 @@ export function AppShell({ children }: { children: React.ReactNode }) {
           <SidebarMenu>
             {menuItems.sort((a,b) => a.href.length - b.href.length).map((item) => (
               <SidebarMenuItem key={item.href}>
-                <Link href={item.href} legacyBehavior passHref>
-                  <SidebarMenuButton
-                    isActive={pathname === item.href}
-                    tooltip={item.label}
-                  >
+                <SidebarMenuButton
+                  asChild
+                  isActive={pathname === item.href}
+                  tooltip={item.label}
+                >
+                  <Link href={item.href}>
                     <item.icon />
                     <span>{item.label}</span>
-                  </SidebarMenuButton>
-                </Link>
+                  </Link>
+                </SidebarMenuButton>
               </SidebarMenuItem>
             ))}
           </SidebarMenu>
